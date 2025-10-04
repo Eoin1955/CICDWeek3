e46ffe3d768f1c210f066c0d37d3c8f855fcf571
eoin ager <g00431577@atu.ie>
2025-10-04 20:14:35 +0100

    added validation for size of string name product and decimal min and how many products

--- src/main/java/ie/atu/week3cicd/ProductController.java
+++ src/main/java/ie/atu/week3cicd/ProductController.java
@@ -29,4 +29,9 @@ public class ProductController {
         mylist.add(myProduct);
         return myProduct;
     }
+
+    @GetMapping("/count")
+    public int count(){
+        return mylist.size();
+    }
 }
--- src/main/java/ie/atu/week3cicd/product.java
+++ src/main/java/ie/atu/week3cicd/product.java
@@ -1,7 +1,9 @@
 package ie.atu.week3cicd;
 
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,7 +11,9 @@ import lombok.Data;
 @AllArgsConstructor
 public class product {
     @NotBlank
+    @Size(min = 1, max = 100)
     private String Productname;
     @Positive
+    @DecimalMin(value = "2")
     private double Productprice;
 }
